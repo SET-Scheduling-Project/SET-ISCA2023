471bca66b258f0849e239a58a044e9b7d4b1cc4a
RyanWeiTHU <weiyc22@mails.tsinghua.edu.cn>
2024-04-10 23:56:18 +0800

    Tidying: bitset

--- include/bitset.h
+++ include/bitset.h
@@ -5,39 +5,40 @@
 #include <cstdint>
 #include <initializer_list>
 #include <iostream>
+#include <limits>
 #include <vector>
 
-#define FOR_BITSET(var, set) for(Bitset::bitlen_t var = set.first(); var != set.max_size(); var = set.next(var))
+#define FOR_BITSET(var, set) for(Bitset::bitlen_t var = set.first(); var != set.size(); var = set.next(var))
 
 #define MAX_BITS_IN_BS 640
 
 class Bitset: private std::bitset<MAX_BITS_IN_BS>{
-public:
-	typedef std::uint16_t bitlen_t;
 private:
 	typedef std::bitset<MAX_BITS_IN_BS> std_bs;
-	// std::int64_t bits[4];
 	Bitset(std_bs&& base);
 public:
+	typedef std::uint16_t bitlen_t;
+
+	static_assert(std::numeric_limits<bitlen_t>::max() >= MAX_BITS_IN_BS,
+		"bitlen_t must be able to hold MAX_BITS_IN_BS, change it to a larger type.");
+
 	Bitset()=default;
 	explicit Bitset(bitlen_t bit);
-	Bitset(std::initializer_list<bitlen_t> bits);
-	Bitset(std::vector<bitlen_t> list);
+	explicit Bitset(std::initializer_list<bitlen_t> bits);
+	explicit Bitset(std::vector<bitlen_t> list);
 	bitlen_t count() const;
-	// Undefined behaviour if bitset is empty.
 	bitlen_t first() const;
-	// Returns 0 if there is no next.
 	bitlen_t next(bitlen_t bit) const;
 	bool contains(bitlen_t bit) const;
 	void set(bitlen_t bit);
 	void reset(bitlen_t bit);
 	void clear();
-	bitlen_t max_size() const;
+	bitlen_t size() const;
 	Bitset& operator|=(const Bitset& other);
 	bool operator==(const Bitset& other) const;
 	//Bitset& operator|=(bitlen_t other);
-	friend std::ostream& operator<<(std::ostream& os, const Bitset& set);
 	friend Bitset operator|(const Bitset& lhs, const Bitset& rhs);
+	friend std::ostream& operator<<(std::ostream& out, const Bitset& set);
 };
 
 #undef MAX_BITS_IN_BS
