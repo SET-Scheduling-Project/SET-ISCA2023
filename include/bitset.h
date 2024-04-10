e471b437a0446e46d9f414516d2394496e56f3ac
RyanWeiTHU <weiyc22@mails.tsinghua.edu.cn>
2024-04-10 23:58:32 +0800

    Added comments: bitset

--- include/bitset.h
+++ include/bitset.h
@@ -1,3 +1,7 @@
+/* This file contains
+ *	Bitset: a wrapper class for std::bitset
+ */
+
 #ifndef BITSET_H
 #define BITSET_H
 
@@ -8,14 +12,28 @@
 #include <limits>
 #include <vector>
 
+
+/* Used for iterating Bitset objects
+ * Example (calculating sum of bitset):
+ *
+ * Bitset b({1,3,6});
+ * int sum = 0;
+ * FOR_BITSET(i, b){
+ *     sum += i;
+ * }
+ * assert(sum == 10);
+ */
 #define FOR_BITSET(var, set) for(Bitset::bitlen_t var = set.first(); var != set.size(); var = set.next(var))
 
+// Maximal number of bits in the bitset. (must be representable by bitlen_t)
 #define MAX_BITS_IN_BS 640
 
+// Wrapper for std::bitset
 class Bitset: private std::bitset<MAX_BITS_IN_BS>{
 private:
 	typedef std::bitset<MAX_BITS_IN_BS> std_bs;
 	Bitset(std_bs&& base);
+
 public:
 	typedef std::uint16_t bitlen_t;
 
@@ -26,6 +44,8 @@ public:
 	explicit Bitset(bitlen_t bit);
 	explicit Bitset(std::initializer_list<bitlen_t> bits);
 	explicit Bitset(std::vector<bitlen_t> list);
+
+	// Wrapper functions:
 	bitlen_t count() const;
 	bitlen_t first() const;
 	bitlen_t next(bitlen_t bit) const;
@@ -38,6 +58,8 @@ public:
 	bool operator==(const Bitset& other) const;
 	//Bitset& operator|=(bitlen_t other);
 	friend Bitset operator|(const Bitset& lhs, const Bitset& rhs);
+
+	// Print to ostream
 	friend std::ostream& operator<<(std::ostream& out, const Bitset& set);
 };
 
